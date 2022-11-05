4d717796e173e1c52fd36ab0b9c2cc72735a7443
Ngoakor12 <ngoakor12@gmail.com>
2022-11-05 21:42:45 +0200

    chore: add prepMessage function

---
+++ src/message.ts
@@ -0,0 +1,18 @@
+function prepMessage(joke: any, quote: any) {
+  const messageHTML = `
+    <h1>Morning Ngoako</h1>
+    <h2>Quote:</h2>
+    <p>${quote && quote?.quoteHTML}</p>
+    <h2>Joke:</h2>
+    <p>${joke && joke?.jokeHTML}</p>
+  `;
+  const messageText = `
+    Morning Ngoako
+    Quote:
+    ${quote && quote?.quoteHTML}
+    Joke:
+    ${joke && joke?.jokeHTML}
+  `;
+
+  return { messageText, messageHTML };
+}
