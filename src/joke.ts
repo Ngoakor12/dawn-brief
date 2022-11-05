269444ec9d61c44587c048b4ff986177a4ec3e72
Ngoakor12 <ngoakor12@gmail.com>
2022-11-05 21:31:18 +0200

    chore: add working getJoke function

---
+++ src/joke.ts
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+type Joke = {
+  jokeText?: string;
+  jokeHTML?: string;
+};
+
+async function getJoke(): Promise<Joke> {
+  const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist";
+
+  const response = await axios.get(url);
+  const data = await response.data;
+  let jokeText: string;
+  let jokeHTML: string;
+
+  if (data.type === "twopart") {
+    jokeText = `${data.setup}\n${data.delivery}`;
+    jokeHTML = `<p>${jokeText}</p>`;
+  } else {
+    jokeText = data.joke;
+    jokeHTML = jokeText;
+  }
+  return { jokeText, jokeHTML };
+}
+
+// getJoke().then((res) => console.log(res));
+module.exports = getJoke;
